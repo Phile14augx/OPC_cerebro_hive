7469cc260bb690d70c949c996e9d8afe1d508998
Philemon V Nath <philemonvnath@gmail.com>
2026-07-13 19:40:16 +0530

    Configure GitHub Pages deployment

--- OPC/cerebro-hive-website/next.config.ts
+++ OPC/cerebro-hive-website/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let repo = "OPC_cerebro_hive";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true, // Required for static export in Next.js
+  },
+  basePath: isGithubActions ? `/${repo}` : '',
+  assetPrefix: isGithubActions ? `/${repo}/` : '',
 };
 
 export default nextConfig;
