eb17950d333fbfc199bd6ef229f63305d63381d6
Philemon ViswaNath <ravananviswanath@gmail.com>
2026-08-15 14:41:00 +0530

    fix(terraform): restore database credential output contract

--- OPC/cerebro-hive-website/infra/terraform/modules/database/outputs.tf
+++ OPC/cerebro-hive-website/infra/terraform/modules/database/outputs.tf
@@ -1,6 +1,32 @@
-output "endpoint" { value = aws_db_instance.main.endpoint }
-output "address" { value = aws_db_instance.main.address }
-output "port" { value = aws_db_instance.main.port }
-output "db_name" { value = aws_db_instance.main.db_name }
-output "secret_arn" { value = aws_secretsmanager_secret.db.arn }
-output "kms_key_arn" { value = aws_kms_key.rds.arn }
+output "endpoint" {
+  value = aws_db_instance.main.endpoint
+}
+
+output "address" {
+  value = aws_db_instance.main.address
+}
+
+output "port" {
+  value = aws_db_instance.main.port
+}
+
+output "db_name" {
+  value = aws_db_instance.main.db_name
+}
+
+output "username" {
+  value = aws_db_instance.main.username
+}
+
+output "password" {
+  value     = random_password.db.result
+  sensitive = true
+}
+
+output "secret_arn" {
+  value = aws_secretsmanager_secret.db.arn
+}
+
+output "kms_key_arn" {
+  value = aws_kms_key.rds.arn
+}
